a221adb806dc10cfa1776839247df0baded2cef8
donaldssh <donald.shenaj@studenti.unipd.it>
2021-01-12 13:21:44 +0100

    improved formatting

--- bot.py
+++ bot.py
@@ -13,91 +13,90 @@ url = "https://mensamurialdo.it/menu-settimanale/"
 page = requests.get(url)
 html = page.text
 
-soup = BeautifulSoup(html,'html5lib')
-today = date.today().strftime("%Y%m%d")   # example "20200110"
+soup = BeautifulSoup(html, "html5lib")
 
+# example "20200110"
+today = date.today().strftime("%Y%m%d")
+
+# flag == 0 mensa is closed
 flag = 0
-if soup.find(id=today):   # flag == 0 mensa is closed
-    day = soup.find(id=today)  # find the current day
-    menu = day('h3')  # for each day extract the "primo secondo e contorno"
-    piatti = day('span') # extract every piatto
-    flag = 1;
+if soup.find(id=today):
+
+    # find the current day
+    day = soup.find(id=today)
+
+    # for each day extract "primo secondo and contorno"
+    menu = day("h3")
+
+    # extract every plates
+    plates = day("span")
+
+    # flag == 1 mensa is open
+    flag = 1
+
 
 @bot.message_handler(commands=["start"])
 def send_welcome(message):
-        markup = types.ReplyKeyboardMarkup(row_width=2)
-        itembtn1 = types.KeyboardButton('/primi')
-        itembtn2 = types.KeyboardButton('/secondi')
-        itembtn3 = types.KeyboardButton('/contorni')
-        markup.add(itembtn1, itembtn2, itembtn3)
-        bot.reply_to(message, "Scegli una portata:", reply_markup=markup)
+    markup = types.ReplyKeyboardMarkup(row_width=2)
+    itembtn1 = types.KeyboardButton("/primi")
+    itembtn2 = types.KeyboardButton("/secondi")
+    itembtn3 = types.KeyboardButton("/contorni")
+    markup.add(itembtn1, itembtn2, itembtn3)
+    bot.reply_to(message, "Scegli una portata:", reply_markup=markup)
+
 
 @bot.message_handler(commands=["help"])
 def send_welcome(message):
-        string_to_print="I dati sono estratti da mensamurialdo.it\n"
-        string_to_print=string_to_print+"@MurialdoBot è stato sviluppato da @Lombax con il contributo di @DanieleFoscarin.\n"
-        string_to_print=string_to_print+"Istruzioni: \n Primi: /primi\n Secondi: /secondi\n Contorni: /contorni"
-        bot.reply_to(message, string_to_print)
+    welcome_message = (
+        "I dati sono estratti da mensamurialdo.it\n"
+        + "@MurialdoBot è stato sviluppato da @Lombax con il contributo di @DanieleFoscarin.\n"
+        + "Istruzioni: \n Primi: /primi\n Secondi: /secondi\n Contorni: /contorni"
+    )
+    bot.reply_to(message, welcome_message)
+
 
 @bot.message_handler(commands=["primi"])
 def send_welcome(message):
     if flag:
-    
-        string_to_print = find_string_to_print(0)           # 0 -> primi
-                
-        bot.reply_to(message, string_to_print)
-      
+        bot.reply_to(message, get_plates_from_index(0))
     else:
-        bot.reply_to(message, 'mensa chiusa')
-      
-      
+        bot.reply_to(message, "mensa chiusa")
+
+
 @bot.message_handler(commands=["secondi"])
 def send_welcome(message):
     if flag:
-        
-        string_to_print = find_string_to_print(1)           # 1 -> secondi
-                
-        bot.reply_to(message, string_to_print)
-        
+        bot.reply_to(message, get_plates_from_index(1))
     else:
-        bot.reply_to(message, 'mensa chiusa')
-            
-            
+        bot.reply_to(message, "mensa chiusa")
+
+
 @bot.message_handler(commands=["contorni"])
 def send_welcome(message):
     if flag:
-        
-        string_to_print = find_string_to_print(2)           # 2 -> contorni/dolci
-                
-        bot.reply_to(message, string_to_print)
-        
+        bot.reply_to(message, get_plates_from_index(2))
+
     else:
-        bot.reply_to(message, 'mensa chiusa')
-        
-        
-def handle_messages(messages):
-    for message in messages:
-        file = open("users.txt", "a")
-        file.write(message.chat.username+"\n")
-        file.close()
-   
-   
-def find_string_to_print(i):
-        nome_piatto = menu[i].contents[0]
-        string_to_print = nome_piatto+"\n"
-        start = i*16
-        end = 16*(i+1)
-        for j in range(start, end):   # 8 varieta per piatto
-            piatto = piatti[j]
-            string_to_print = string_to_print + piatto.contents[0]+'\n'
-        
-        return string_to_print
-            
-            
+        bot.reply_to(message, "mensa chiusa")
+
+
+def get_plates_from_index(i):
+    plate_name = menu[i].contents[0]
+    formatted_plates = plate_name + "\n"
+    start = i * 16
+    end = 16 * (i + 1)
+
+    # 8 kinds of plates
+    for j in range(start, end):
+        plate = plates[j]
+        formatted_plates += plate.contents[0] + "\n"
+
+    return formatted_plates
+
+
 while True:
     try:
-        bot.set_update_listener(handle_messages)
-        bot.polling()   
-        
+        bot.polling()
+
     except Exception:
         time.sleep(15)
--- today-menu.py
+++ today-menu.py
@@ -1,6 +1,3 @@
-# dipendenze:
-# pip install beautifulsoup4
-
 import requests
 from bs4 import BeautifulSoup
 from datetime import date
@@ -11,30 +8,28 @@ url = "https://mensamurialdo.it/menu-settimanale/"
 page = requests.get(url)
 html = page.text
 
-soup = BeautifulSoup(html,'html5lib')
+soup = BeautifulSoup(html, "html5lib")
 
-today = date.today().strftime("%Y%m%d")   # example "20200110"
+today = date.today().strftime("%Y%m%d")
 
 flag = 0
-if soup.find(id=today):   # flag == 0 se la mensa e' chiusa
-    day = soup.find(id=today)  # find the current day
-    menu = day('h3')  # for each day extract the "primo secondo e contorno"
-    piatti = day('span') # extract every piatto
-    flag = 1;
+if soup.find(id=today):
+    day = soup.find(id=today)
+    menu = day("h3")
+    plates = day("span")
+    flag = 1
 
 if flag == 0:
     print(date.today().strftime("%d-%m-%Y") + " mensa chiusa")
     sys.exit(0)
 
 
-for i in range(3):   # primo secondo e contorno
-    nome_piatto = menu[i].contents[0]
-    print(nome_piatto)
+for i in range(3):
+    plate_name = menu[i].contents[0]
+    print(plate_name)
 
-    start = i*16
-    end = 16*(i+1)
-    for j in range(start, end):   # 8 varieta per piatto
-        piatto = piatti[j]
-        print(piatto.contents[0])
-        
-
+    start = i * 16
+    end = 16 * (i + 1)
+    for j in range(start, end):
+        plate = plates[j]
+        print(plate.contents[0])

a5931b58e21d0d6056f981d1f10d4cfcf6b2c085
donaldssh <donald.shenaj@studenti.unipd.it>
2021-04-22 18:14:33 +0200

    refactoring

--- bot.py
+++ bot.py
@@ -4,6 +4,7 @@ from datetime import date
 import telebot
 from telebot import types
 import sys
+import time
 
 bot_token = sys.argv[1]
 bot = telebot.TeleBot(token=bot_token)
@@ -16,6 +17,8 @@ soup = BeautifulSoup(html, "html5lib")
 
 mensa_closed_message = "Mensa chiusa"
 
+meal_courses = ["primi", "secondi", "contorni"]
+
 
 def get_date():
     return date.today().strftime("%Y%m%d")
@@ -25,17 +28,17 @@ def mensa_is_open():
     return 1 if soup.find(id=get_date()) else 0
 
 
-def get_plates_from_index(i):
-    formatted_plates = ""
-    plates = soup.find(id=get_date()).find_all(class_="col")[i].find_all("span")
-    for plate in plates:
-        formatted_plates += str(plate.text) + "\n"
+def get_dishes_from_index(i):
+    formatted_dishes = ""
+    dishes = soup.find(id=get_date()).find_all(class_="col")[i].find_all("span")
+    for dish in dishes:
+        formatted_dishes += str(dish.text) + "\n"
 
-    return formatted_plates
+    return formatted_dishes
 
 
 @bot.message_handler(commands=["start"])
-def send_welcome(message):
+def send_start(message):
     markup = types.ReplyKeyboardMarkup(row_width=2)
     itembtn1 = types.KeyboardButton("/primi")
     itembtn2 = types.KeyboardButton("/secondi")
@@ -44,7 +47,7 @@ def send_welcome(message):
     bot.reply_to(message, "Scegli una portata:", reply_markup=markup)
 
 
-@bot.message_handler(commands=["help"])
+@bot.message_handler(commands=["help", "info"])
 def send_welcome(message):
     welcome_message = (
         "I dati sono estratti da mensamurialdo.it\n"
@@ -54,26 +57,11 @@ def send_welcome(message):
     bot.reply_to(message, welcome_message)
 
 
-@bot.message_handler(commands=["primi"])
-def send_welcome(message):
+@bot.message_handler(commands=meal_courses)
+def send_dish(message):
+    meal = message.text.split("/")[1]
     if mensa_is_open():
-        bot.reply_to(message, get_plates_from_index(0))
-    else:
-        bot.reply_to(message, mensa_closed_message)
-
-
-@bot.message_handler(commands=["secondi"])
-def send_welcome(message):
-    if mensa_is_open():
-        bot.reply_to(message, get_plates_from_index(1))
-    else:
-        bot.reply_to(message, mensa_closed_message)
-
-
-@bot.message_handler(commands=["contorni"])
-def send_welcome(message):
-    if mensa_is_open():
-        bot.reply_to(message, get_plates_from_index(2))
+        bot.reply_to(message, get_dishes_from_index(meal_courses.index(meal)))
     else:
         bot.reply_to(message, mensa_closed_message)
 
